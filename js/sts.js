91c63492a2ee3e1d250c1ddf18d587515bfd73b3
Schatzkin <schatzkin@eff.org>
2018-06-25 16:53:58 -0700

    count only once for alpr, need to extrapolate for all objects

--- js/sts.js
+++ js/sts.js
@@ -12,11 +12,21 @@
 		  init: function () {
 		    var sceneEl = document.querySelector('a-scene');
 		    var scoreBoard = document.querySelector('#count-display');
+				var alpr = sceneEl.querySelector('#alpr-hotspot');
+				var elem = document.getElementById("alpr-hotspot");
 
-		    sceneEl.querySelector('.hotspot').addEventListener('click', () => {
-		      this.data.score++;
-		      var newScore = 'You have found: ' + this.data.score + 'devices.'
-		      scoreBoard.setAttribute('text', 'value',  newScore)
+
+		    sceneEl.querySelector('#alpr-hotspot').addEventListener('click', () => {
+
+		     this.data.score++;
+				 currentScore = this.data.score;
+					if (currentScore == 1) {
+						var newScore = 'You have found 1 device.'
+					} else {
+		      	var newScore = 'You have found: ' + this.data.score + ' devices.'
+					}
+		      scoreBoard.setAttribute('text', 'value',  newScore);
+					elem.parentNode.removeChild(elem);
 		    })
 		  }
 		});
