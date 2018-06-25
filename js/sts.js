2ac5787064ad0418dd753f79e754364aa3a121b1
Schatzkin <schatzkin@eff.org>
2018-06-25 13:59:19 -0700

    basic counter working

--- js/sts.js
+++ js/sts.js
@@ -1,16 +1,22 @@
-AFRAME.registerComponent('click-test', {
-	    init: function () {
-            const sphere = document.querySelector('.hotspot')
-            //this.x = 0;
-            var clickCounter = 0;
-            sphere.addEventListener('click', () => {
-                console.log('sphere click');
-                //console.log(this.x++);
-                clickCounter++;
-                console.log(clickCounter);
-				var countValue = document.getElementById("count-display");
-				alert("count display= " countValue);
-				document.getElementById("count-display").setAttribute("value","Counter: " + clickCounter);
-            })
-        }
-    });
+		AFRAME.registerComponent('click-test', {
+		  schema: {
+		    el: {
+		      type: 'selector'
+		    },
+		    score:{
+		      type: 'int',
+		      default: 0
+		    },
+		  },
+
+		  init: function () {
+		    var sceneEl = document.querySelector('a-scene');
+		    var scoreBoard = document.querySelector('#count-display');
+
+		    sceneEl.querySelector('.hotspot').addEventListener('click', () => {
+		      this.data.score++;
+		      var newScore = 'Score: ' + this.data.score
+		      scoreBoard.setAttribute('text', 'value',  newScore)
+		    })
+		  }
+		});
