538472587129145055473d5809c92e3fd51c9848
Schatzkin <schatzkin@eff.org>
2018-06-28 17:10:58 -0700

    working on only letting users click each object once

--- js/sts.js
+++ js/sts.js
@@ -1,32 +1,33 @@
-		AFRAME.registerComponent('click-test', {
-		  schema: {
-		    el: {
-		      type: 'selector'
-		    },
-		    score:{
-		      type: 'int',
-		      default: 0
-		    },
-		  },
+AFRAME.registerComponent('click-test', {
+	schema: {
+		el: {
+			type: 'selector'
+		},
+		score:{
+			type: 'int',
+			default: 0
+		},
+	},
 
-		  init: function () {
-		    var sceneEl = document.querySelector('a-scene');
-		    var scoreBoard = document.querySelector('#count-display');
-				var alpr = sceneEl.querySelector('#alpr-hotspot');
-				var elem = document.getElementById("alpr-hotspot");
+	init: function () {
+		var sceneEl = document.querySelector('a-scene');
+		var scoreBoard = document.querySelector('#count-display');
 
+	//	var elem = document.getElementById("alpr-hotspot");
 
-		    sceneEl.querySelector('#alpr-hotspot').addEventListener('click', () => {
-
-		     this.data.score++;
-				 currentScore = this.data.score;
-					if (currentScore == 1) {
-						var newScore = 'You have found 1 device.'
-					} else {
-		      	var newScore = 'You have found: ' + this.data.score + ' devices.'
-					}
-		      scoreBoard.setAttribute('text', 'value',  newScore);
-					elem.parentNode.removeChild(elem);
-		    })
-		  }
-		});
+		sceneEl.querySelector('.hotspot').addEventListener('click', () => {
+			clickID = sceneEl.querySelector('.hotspot').id;
+			alert(clickID);
+			this.data.score++;
+			currentScore = this.data.score;
+			if (currentScore == 1) {
+				var newScore = 'You have found 1 device.'
+			} else {
+				var newScore = 'You have found: ' + this.data.score + ' devices.'
+			}
+			scoreBoard.setAttribute('text', 'value',  newScore);
+			var elem = document.getElementById(clickID);
+			elem.parentNode.removeChild(elem);
+		})
+	}
+});
