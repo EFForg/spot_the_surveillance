819699365ed027f8a5a0953f14c0112e955d8d83
Schatzkin <schatzkin@eff.org>
2018-08-10 13:14:20 -0700

    removing extraneous code

--- js/replay.js
+++ js/replay.js
@@ -3,7 +3,6 @@ AFRAME.registerComponent('replay', {
     var scene = document.querySelector('a-scene');
     this.el.addEventListener('click', evt => {
       location.reload(true);
-      setTimeout(gameover, 2000);
       scene.enterVR();
     });
   }
