c6020a5eefacdaee216365b2f8e9ec92cd36b02a
Schatzkin <schatzkin@eff.org>
2018-08-10 11:26:25 -0700

    hiding all popups after congrats cards appear

--- js/sts.js
+++ js/sts.js
@@ -5,6 +5,18 @@ AFRAME.registerComponent('listener', {
 		var congrats1 = document.querySelector('#congrats-popup1');
 		var congrats2 = document.querySelector('#congrats-popup2');
 		var congrats3 = document.querySelector('#congrats-popup3');
+		// all other popups
+		var alpr = document.querySelector('#alpr-popup');
+		var alprMobile = document.querySelector('#alpr-mobile-popup');
+		var biometric = document.querySelector('#biometric-popup');
+		var bodyCam = document.querySelector('#bodycam-popup');
+		var camera1 = document.querySelector('#camera1-popup');
+		var camera2 = document.querySelector('#camera2-popup');
+		var drone = document.querySelector('#drone-popup');
+		var bird = document.querySelector('#bird-popup');
+		var light = document.querySelector('#light-popup');
+		var intro = document.querySelector('#intro-popup');
+
 
 		this.el.addEventListener('mouseenter', evt => {
 			if (evt.target.dataset.wasClicked) {
@@ -12,28 +24,33 @@ AFRAME.registerComponent('listener', {
 			}
 			// evt.target tells you which was clicked.
 			evt.target.dataset.wasClicked = true;
-			//alert('evt.target.id);
 			// Score up.
 			var scoreBoard = document.querySelector('#count-display');
 			score++;
 			var newScore = score + ' / 7'
 			scoreBoard.setAttribute('text', 'value',  newScore);
-			if (score > 1) {
-
+			if (score == 7) {
+				// remove all other popups
+				function hidePopups() {
+					console.log('hidePopups');
+					alpr.setAttribute('visible', false);
+					alprMobile.setAttribute('visible', false);
+					biometric.setAttribute('visible', false);
+					bodyCam.setAttribute('visible', false);
+					camera1.setAttribute('visible', false);
+					camera2.setAttribute('visible', false);
+					drone.setAttribute('visible', false);
+					bird.setAttribute('visible', false);
+					light.setAttribute('visible', false);
+					intro.setAttribute('visible', false);
+				}
 				function tada() {
-				console.log("Congrats!");
 				congrats1.setAttribute('visible', true);
 				congrats2.setAttribute('visible', true);
 				congrats3.setAttribute('visible', true);
-
-			//	function gameover() {
-			//		scene.reload();
-			//	}
 			}
-
 			setTimeout(tada, 10000);
-			//setTimeout(gameover, 10000)
-
+			setTimeout(hidePopups, 20000);
 			}
 		});
 	}
