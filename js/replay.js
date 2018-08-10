793a1d290401c9119e9ab2c2b420cf663df05d8f
Schatzkin <schatzkin@eff.org>
2018-08-10 11:29:05 -0700

    removing timeout on replay

--- js/replay.js
+++ js/replay.js
@@ -1,14 +1,10 @@
 AFRAME.registerComponent('replay', {
-	init: function () {
-	  var scene = document.querySelector('a-scene');
-		this.el.addEventListener('click', evt => {
-        console.log('clicked');
-				function gameover() {
-					location.reload(true);
-				}
-        	setTimeout(gameover, 2000);
-          scene.enterVR();
-
-		});
-	}
+  init: function () {
+    var scene = document.querySelector('a-scene');
+    this.el.addEventListener('click', evt => {
+      location.reload(true);
+      setTimeout(gameover, 2000);
+      scene.enterVR();
+    });
+  }
 });
