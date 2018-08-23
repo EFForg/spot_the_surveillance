d5c769ed690f09475cda8185040104b8ce8c48a6
Schatzkin <schatzkin@eff.org>
2018-08-23 12:24:44 -0700

    click once working correctly

--- js/clickonce.js
+++ js/clickonce.js
@@ -2,16 +2,15 @@ AFRAME.registerComponent('click-once', {
   init: function () {
     var self = this;
     var scene = self.el.sceneEl;
-    var raycaster = scene.querySelector('#cursor').components.raycaster;
-
+    var raycaster = scene.querySelector('[cursor]').components.raycaster;
     // Define function to be executed on click.
     var clickHandler = function (e) {
       // Log clicks.
       console.log('clicked');
       // Remove "clickable" class from entity.
-      self.el.classList.remove('clickable');
+      self.el.removeAttribute('data-clickable');
       // Refresh raycaster object list to reflect changes.
-      raycaster.refreshObjects();
+      //raycaster.refreshObjects();
       // Remove event listener since no longer needed.enter
       self.el.removeEventListener('click', clickHandler);
     };
