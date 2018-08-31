4de6aa5fad07677abfa9ed8d0fbe95a6e71a719c
Schatzkin <schatzkin@eff.org>
2018-08-31 10:05:45 -0700

    working on ensuring all assets are loaded when entering scene

--- js/load-scene.js
+++ js/load-scene.js
@@ -1,7 +1,11 @@
 AFRAME.registerComponent('loadscene', {
   init: function () {
     this.el.addEventListener("click", (e)=> {
+      document.querySelector('a-assets').addEventListener('loaded', function () {
+        console.log("OK LOADED");
+      });
       document.getElementById('sts-live').setAttribute('visible', true);
     });
+
   }
 });
