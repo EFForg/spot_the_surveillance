e45ac4638e0ff85619d8a920f27782b4be30524a
Schatzkin <schatzkin@eff.org>
2018-09-05 14:45:52 -0700

    added load to hotspot audio

--- js/audio-hotspot.js
+++ js/audio-hotspot.js
@@ -5,6 +5,7 @@ for (var i=0; i < el.length; i++) {
     // Here we have the same onclick
     el.item(i).onclick = function() {
        console.log('Click just happened');
+          ding.load();
           ding.play();
     };
 }
