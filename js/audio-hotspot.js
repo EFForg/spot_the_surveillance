f237a914485bb94118d0e129b6ab626ccd726d0c
Schatzkin <schatzkin@eff.org>
2018-09-05 16:49:50 -0700

    removing console log

--- js/audio-hotspot.js
+++ js/audio-hotspot.js
@@ -2,7 +2,7 @@ var ding = document.getElementById("ding");
 var hotspots = document.getElementById("surveillance-objects");
 
 hotspots.addEventListener("mouseenter", function( event ) {
-  console.log('Click just happened');
+  // console.log('Click just happened');
   ding.load();
   ding.play();
 });
