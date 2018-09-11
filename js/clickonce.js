158036bac686663804b3f650583b9c365518b914
Schatzkin <schatzkin@eff.org>
2018-09-11 12:55:58 -0700

    changed clickonce to be mouseenter rather than click, reordered js files in html

--- js/clickonce.js
+++ js/clickonce.js
@@ -5,11 +5,12 @@ AFRAME.registerComponent('click-once', {
     var raycaster = scene.querySelector('[cursor]').components.raycaster;
 
     var clickHandler = function (e) {
+      console.log('remove data-clickable');
       self.el.removeAttribute('data-clickable');
       // Remove event listener since no longer needed.enter
-      self.el.removeEventListener('click', clickHandler);
+      self.el.removeEventListener('mouseenter', clickHandler);
     };
     // Add event listener for click.
-    self.el.addEventListener('click', clickHandler);
+    self.el.addEventListener('mouseenter', clickHandler);
   }
 });
