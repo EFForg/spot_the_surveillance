30127933b1a20fad1414bf8390f0d6704e6fe09a
Schatzkin <schatzkin@eff.org>
2018-09-14 11:21:44 -0700

    commenting out console.log

--- js/load-scene.js
+++ js/load-scene.js
@@ -1,7 +1,7 @@
 AFRAME.registerComponent('loadscene', {
   init: function () {
       document.querySelector('a-scene').addEventListener('loaded', function () {
-        console.log("OK LOADED");
+        // console.log("OK LOADED");
         document.getElementById('intro-audio').play();
         document.getElementById('ambience').play();
         document.getElementById('camera-audio').addEventListener('loaded', function() { alert('ok') })
