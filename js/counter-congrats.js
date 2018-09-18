16295be0c2189aee75d8b92981dcba6328823053
Schatzkin <schatzkin@eff.org>
2018-09-18 14:48:36 -0700

    adding particle system component for confetti

--- js/counter-congrats.js
+++ js/counter-congrats.js
@@ -7,7 +7,7 @@ AFRAME.registerComponent('listener', {
 		var congratsLeft2 = document.querySelector('#congrats-left-arrow2');
 		var congratsRight1 = document.querySelector('#congrats-right-arrow1');
 		var congratsRight2 = document.querySelector('#congrats-right-arrow2');
-		var confetti = document.querySelector('#confetti');
+		//var confetti = document.querySelector('#confetti');
 		// all other popups
 		var alpr = document.querySelector('#alpr-popup');
 		var alprMobile = document.querySelector('#alpr-mobile-popup');
@@ -19,6 +19,7 @@ AFRAME.registerComponent('listener', {
 		var bird = document.querySelector('#bird-popup');
 		var light = document.querySelector('#light-popup');
 		var intro = document.querySelector('#intro-popup');
+
 
 		this.el.addEventListener('mouseenter', (e) => {
 			e.target.setAttribute("visible", "true")
@@ -54,7 +55,29 @@ AFRAME.registerComponent('listener', {
 							  playing = false;
 
 					});
-					//confetti.setAttribute('particle-system:enabled', true);
+					function confetti() {
+					  var sceneEl = document.querySelector('a-scene');
+					  var entityEl = document.createElement('a-entity');
+
+					  // initialize the entity.
+					  sceneEl.appendChild(entityEl);
+					  entityEl.setAttribute('particle-system', {
+					    accelerationSpread: '20 0 20',
+					    color: '#FFFFFF',
+					    enabled: true,
+							maxParticleCount: 80,
+					    particleCount: 40,
+					    opacity: 0.2,
+					    size: 0.5,
+					    velocityValue: '0 15 0'
+					  });
+						entityEl.setAttribute('position', { x: 5.914, y: 1.9, z: -0.927 });
+					  entityEl.setAttribute('material', {
+					    transparent: true,
+					    visible: false
+					  });
+					}
+					confetti();
 				}
 				// remove all other popups
 				function hidePopups() {
