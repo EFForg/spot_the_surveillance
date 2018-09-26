cbb66a1fa8aab8215f90c71670036ca09cc18337
Schatzkin <schatzkin@eff.org>
2018-09-26 14:42:06 -0700

    repositioning some hotspots for desktop (no headset), or else they appear misaligned with their devices

---
+++ js/browser-reposition.js
@@ -0,0 +1,17 @@
+function browserReposition() {
+  // these hotspot appear incorrectly positioned in browser/mobile views
+  var alpr = document.querySelector('#alpr-hotspot');
+  var alprMobile = document.querySelector('#alpr-mobile-hotspot');
+  var bodycam = document.querySelector('#bodycam-hotspot');
+  var camera1 = document.querySelector('#camera1-hotspot');
+  var drone = document.querySelector('#drone-hotspot');
+
+  if ( !AFRAME.utils.device.checkHeadsetConnected() ){
+    alpr.setAttribute('position', { x: 10.379, y: 26.918, z: -7.485 });
+    alprMobile.setAttribute('position', { x: 3.622, y: 2.229, z: 12.881 });
+    bodycam.setAttribute('position', { x: -4.564, y: 0.577, z: -6.036 });
+    camera1.setAttribute('position', { x: 23.244, y: 58.391, z: -35.322 });
+    drone.setAttribute('position', { x: -3.999, y: 30.295, z: 28.948 });
+  }
+}
+browserReposition();
