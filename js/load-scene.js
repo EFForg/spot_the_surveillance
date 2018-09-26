8b7e1113774547d42eefcf054d3ab134e650a55b
Schatzkin <schatzkin@eff.org>
2018-09-26 13:19:04 -0700

    scene loads automatically on mobile (no black bg)

--- js/load-scene.js
+++ js/load-scene.js
@@ -1,13 +1,17 @@
 AFRAME.registerComponent('loadscene', {
   init: function () {
-      document.querySelector('a-scene').addEventListener('loaded', function () {
-        // console.log("OK LOADED");
-        document.getElementById('intro-audio').play();
-        document.getElementById('ambience').play();
-        document.getElementById('camera-audio').addEventListener('loaded', function() { alert('ok') })
-      });
+    document.querySelector('a-scene').addEventListener('loaded', function () {
+      // console.log("OK LOADED");
+      document.getElementById('intro-audio').play();
+      document.getElementById('ambience').play();
+      document.getElementById('camera-audio').addEventListener('loaded', function() { alert('ok') })
+    });
+    if (AFRAME.utils.device.isMobile()){
+      document.getElementById('sts-live').setAttribute('visible', true);
+    } else {
       this.el.addEventListener("click", (e)=> {
         document.getElementById('sts-live').setAttribute('visible', true);
       });
+    }
   }
 });
