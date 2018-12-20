3528ed815241359103664aaa838282872a3e83e6
Schatzkin <schatzkin@eff.org>
2018-12-19 16:19:42 -0800

    changed congrats card to be dynamically loaded when counter=7

--- js/counter-congrats.js
+++ js/counter-congrats.js
@@ -36,46 +36,88 @@ AFRAME.registerComponent('listener', {
 			scoreBoard.setAttribute('text', 'value',  newScore);
 			if (score == 7) {
 				function tada() {
-					congrats.setAttribute('visible', true);
-					congrats.setAttribute('material', { visible: true});
+					var congratsEl = document.createElement('a-sound');
+					// create congrats card
+					function loadCongrats(){
+						var congratsAll = document.querySelector('#congrats-all');
+						var congratsEl = document.createElement('a-sound');
+						congratsEl.setAttribute('geometry', {primitive: 'plane', width: 4, height: 4});
+						congratsEl.setAttribute('material', {shader: 'flat', side: 'front', opacity: 1, transparent: 'true', visible: 'true', src: '#congrats-card'});
+						congratsEl.setAttribute('visible', 'true');
+						congratsEl.setAttribute('position', {x:5.584, y: 1.25, z:-1.325 });
+						congratsEl.setAttribute('rotation', {x: 0, y: -80, z: 0});
+						congratsEl.setAttribute('data-clickable','');
+						congratsEl.setAttribute('play-audio','');
+						congratsEl.setAttribute('autoplay', 'false');
+						congratsEl.setAttribute('loop', 'false');
+						congratsEl.setAttribute('volume', 5);
+						congratsEl.setAttribute('src','#congrats-audio');
+						congratsEl.id = "congrats-popup";
+						congratsAll.appendChild(congratsEl);``
+
+						// create sls button
+						var slsEl = document.createElement('a-entity');
+						slsEl.setAttribute('geometry', {primitive: 'plane', height:0.5, width: 2.1, });
+						slsEl.setAttribute('material', {shader: 'flat', side: 'front', opacity: 0, transparent: 'true', visible: 'false'});
+						slsEl.setAttribute('visible', 'false');
+						slsEl.setAttribute('position', {x:0, y: -0.658, z: 0.399 });
+						slsEl.setAttribute('data-clickable','');
+						slsEl.setAttribute('exitlink',"href:https://www.eff.org/issues/street-level-surveillance")
+						slsEl.id = "sls1";
+						congratsEl.appendChild(slsEl);
+
+						// create replay button
+						var replayEl = document.createElement('a-entity');
+						replayEl.setAttribute('geometry', {primitive: 'plane', height:0.35, width: 1.2, });
+						replayEl.setAttribute('material', {shader: 'flat', side: 'front', opacity: 0, transparent: 'true', visible: 'false'});
+						replayEl.setAttribute('visible', 'false');
+						replayEl.setAttribute('position', {x:-0.010, y: -1.525, z: 0.074 });
+						replayEl.setAttribute('data-clickable','');
+						replayEl.setAttribute('replay','');
+						replayEl.id = "replay1";
+						congratsEl.appendChild(replayEl);
+					}
+
+					loadCongrats();
+
 					congratsLeft1.setAttribute('visible', true);
 					congratsLeft2.setAttribute('visible', true);
 					congratsRight1.setAttribute('visible', true);
 					congratsRight2.setAttribute('visible', true);
-					congratsAudio = congrats.components.sound;
+					congratsAudio = congratsEl.components.sound;
 					let playing = false;
-					congrats.addEventListener('mouseenter', () => {
-						  if (!playing) {
-								congratsAudio.playSound();
-								playing = true;
-							}
+					congratsEl.addEventListener('mouseenter', () => {
+						if (!playing) {
+							congratsAudio.playSound();
+							playing = true;
+						}
 					});
-					congrats.addEventListener('mouseleave', () => {
-						    congratsAudio.stopSound();
-							  playing = false;
+					congratsEl.addEventListener('mouseleave', () => {
+						congratsAudio.stopSound();
+						playing = false;
 
 					});
 					function confetti() {
-					  var sceneEl = document.querySelector('a-scene');
-					  var entityEl = document.createElement('a-entity');
+						var sceneEl = document.querySelector('#sts-live');
+						var confettiEl = document.createElement('a-entity');
 
-					  // initialize the entity.
-					  sceneEl.appendChild(entityEl);
-					  entityEl.setAttribute('particle-system', {
-					    accelerationSpread: '20 0 20',
-					    color: '#FFFFFF',
-					    enabled: true,
+						confettiEl.setAttribute('particle-system', {
+							accelerationSpread: '20 0 20',
+							color: '#FFFFFF',
+							enabled: true,
 							maxParticleCount: 80,
-					    particleCount: 40,
-					    opacity: 0.2,
-					    size: 0.5,
-					    velocityValue: '0 15 0'
-					  });
-						entityEl.setAttribute('position', { x: 5.914, y: 1.9, z: -0.927 });
-					  entityEl.setAttribute('material', {
-					    transparent: true,
-					    visible: false
-					  });
+							particleCount: 40,
+							opacity: 0.2,
+							size: 0.5,
+							velocityValue: '0 15 0'
+						});
+						confettiEl.setAttribute('position', { x: 5.914, y: 1.9, z: -0.927 });
+						confettiEl.setAttribute('material', {
+							transparent: true,
+							visible: false
+						});
+						confettiEl.id = "confetti";
+						sceneEl.appendChild(confettiEl);
 					}
 					confetti();
 				}
@@ -93,7 +135,7 @@ AFRAME.registerComponent('listener', {
 					intro.setAttribute('visible', false);
 				}
 				setTimeout(tada, 5000);
-				setTimeout(hidePopups, 10000);
+				setTimeout(hidePopups, 20000);
 			}
 		});
 	}
