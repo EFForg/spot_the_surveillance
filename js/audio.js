497eb02fa9825ef1c6443044e81161df08b21a50
Schatzkin <schatzkin@eff.org>
2019-01-07 16:41:03 -0800

    making sure object is visible before pausing; making sure volumen is on when visible

--- js/audio.js
+++ js/audio.js
@@ -7,10 +7,11 @@ AFRAME.registerComponent('play-audio', {
 
     self.el.addEventListener('mouseenter', () => {
       var viz = self.el.getAttribute('material').visible;
-      //console.log('vis=' + viz);
+      // console.log('viz=' + viz);
       if (viz){
         if(!playing) {
-          // console.log('playing');
+           // console.log('playing');
+           self.el.setAttribute('sound', 'volume', '5');
           audio.playSound();
         //  self.el.removeAttribute('data-clickable');
         } else {
@@ -23,9 +24,13 @@ AFRAME.registerComponent('play-audio', {
 
     });
     self.el.addEventListener('mouseleave', () => {
+      var viz2 = self.el.getAttribute('material').visible;
+      // console.log('viz2=' + viz2);
+      if (viz2){
       console.log('pausing');
       audio.pauseSound();
       playing = false;
+    }
     })
   }
 })
