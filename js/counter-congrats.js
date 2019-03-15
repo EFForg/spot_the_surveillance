6bdcea0ad2cfa1918a3e767a10c5423257bdec56
Schatzkin <schatzkin@eff.org>
2019-03-15 14:20:38 -0700

    adjusting congrats card position

--- js/counter-congrats.js
+++ js/counter-congrats.js
@@ -47,7 +47,7 @@ AFRAME.registerComponent('listener', {
 						congratsEl.setAttribute('geometry', {primitive: 'plane', width: 4, height: 4});
 						congratsEl.setAttribute('material', {shader: 'flat', side: 'front', opacity: 1, transparent: 'true', visible: 'true', src: '#congrats-card'});
 						congratsEl.setAttribute('visible', 'true');
-						congratsEl.setAttribute('position', {x:-6.202, y: 2.028, z:4.658 });
+						congratsEl.setAttribute('position', {x: -5.573, y: 1.988, z: 4.858 });
 						congratsEl.setAttribute('rotation', {x: 0, y: 110, z: 0});
 						congratsEl.setAttribute('data-clickable','');
 						congratsEl.setAttribute('sound','src: #congrats-audio; volume: 5; loop: false; autoplay: false');
