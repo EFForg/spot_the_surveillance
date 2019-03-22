d434a7ca1f6f479ad11f4c0dd7ff0fae95ccccb1
Schatzkin <schatzkin@eff.org>
2019-03-22 15:37:01 -0700

    updated aframe to 0.9.0, updated aframe-event-set-component

--- js/aframe-event-set-component.js
+++ js/aframe-event-set-component.js
@@ -1,41 +1,41 @@
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
-
+/******/
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
-
+/******/
 /******/ 		// Check if module is in cache
 /******/ 		if(installedModules[moduleId])
 /******/ 			return installedModules[moduleId].exports;
-
+/******/
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
 /******/ 			exports: {},
 /******/ 			id: moduleId,
 /******/ 			loaded: false
 /******/ 		};
-
+/******/
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-
+/******/
 /******/ 		// Flag the module as loaded
 /******/ 		module.loaded = true;
-
+/******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-
-
+/******/
+/******/
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = modules;
-
+/******/
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
-
+/******/
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
-
+/******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
 /******/ })
@@ -103,7 +103,8 @@
 	    targetEl = target ? el.sceneEl.querySelector(target) : el;
 
 	    this.eventName = event;
-	    this.eventHandler = function handler () {
+
+	    const handler = () => {
 	      var propName;
 	      // Set attributes.
 	      for (propName in data) {
@@ -112,6 +113,13 @@
 	                                                      data[propName]);
 	      }
 	    };
+
+	    if (!isNaN(data._delay)) {
+	      // Delay.
+	      this.eventHandler = () => { setTimeout(handler, parseFloat(data._delay)); };
+	    } else {
+	      this.eventHandler = handler;
+	    }
 	  },
 
 	  addEventListener: function () {
