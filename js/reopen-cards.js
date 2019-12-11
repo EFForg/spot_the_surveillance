b114c1a4f38bf79905ecefb35c866098635acc84
Laura Schatzkin <schatzkin@eff.org>
2019-12-11 14:34:41 -0800

    shot spotter card reopens when reopen all cards

--- js/reopen-cards.js
+++ js/reopen-cards.js
@@ -6,8 +6,8 @@ AFRAME.registerComponent('reopen-cards', {
 		var alprMobile = document.querySelector('#alpr-mobile-popup');
 		var biometric = document.querySelector('#biometric-popup');
 		var bodyCam = document.querySelector('#bodycam-popup');
-		var camera1 = document.querySelector('#camera1-popup');
-		var camera2 = document.querySelector('#camera2-popup');
+		var camera = document.querySelector('#camera-popup');
+		var shotSpotter = document.querySelector('#shot-spotter-popup');
 		var drone = document.querySelector('#drone-popup');;
     el.addEventListener('mouseenter', function () {
           console.log('reopen cards');
@@ -15,8 +15,8 @@ AFRAME.registerComponent('reopen-cards', {
         alprMobile.setAttribute('visible', true);
         biometric.setAttribute('visible', true);
         bodyCam.setAttribute('visible', true);
-        camera1.setAttribute('visible', true);
-        camera2.setAttribute('visible', true);
+        camera.setAttribute('visible', true);
+        shotSpotter.setAttribute('visible', true);
         drone.setAttribute('visible', true);
     });
   }
