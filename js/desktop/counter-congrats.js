e5e838ede4e0955cfc9f6a12005a821e6fc16fa5
Artemis Schatzkin <schatzkin@eff.org>
2020-09-21 11:21:39 -0700

    reposition reopen; unable to get working on click

--- js/desktop/counter-congrats.js
+++ js/desktop/counter-congrats.js
@@ -94,17 +94,14 @@ AFRAME.registerComponent('listener', {
 						var reopenEl = document.createElement('a-entity');
 						reopenEl.setAttribute('geometry', {primitive: 'plane', height:0.75, width: 2.1, });
 						reopenEl.setAttribute('material', {shader: 'flat', side: 'front', opacity: 1, transparent: 'true', visible: 'true', src: '#reopen'});
+						reopenEl.setAttribute('data-clickable','');
 						// for desktop/mobile
 						if ( !AFRAME.utils.device.checkHeadsetConnected() ){
-							reopenEl.setAttribute('position', {x: -4.436, y: 4.251, z: 4.1 })
-						}
-						else {
-							reopenEl.setAttribute('position', {x: -5.166, y: 4.251, z: 3.559 })
+							reopenEl.setAttribute('position', {x:	-2.272, y: 3.397, z: 4.1 })
 						}
 
-						reopenEl.setAttribute('rotation', {x: 0, y: 110, z: 0});
+						reopenEl.setAttribute('rotation', {x: 0, y: 150, z: 0});
 						reopenEl.setAttribute('visible', false);
-						reopenEl.setAttribute('data-clickable','');
 						reopenEl.setAttribute('reopen-cards','');
 						reopenEl.id = "reopen-cards";
 						congratsAll.appendChild(reopenEl);
@@ -119,7 +116,7 @@ AFRAME.registerComponent('listener', {
 								playing = true;
 							}
 						});
-						congratsEl.addEventListener('mouseleave', () => {
+						congratsEl.addEventListener('click', () => {
 							congratsAudio.pause();
 							playing = false;
 
---
+++ js/desktop/reopen-cards.js
@@ -0,0 +1,23 @@
+AFRAME.registerComponent('reopen-cards', {
+  init: function () {
+    var el = this.el;
+    // all popups
+		var alpr = document.querySelector('#alpr-popup');
+		var alprMobile = document.querySelector('#alpr-mobile-popup');
+		var biometric = document.querySelector('#biometric-popup');
+		var bodyCam = document.querySelector('#bodycam-popup');
+		var camera = document.querySelector('#camera-popup');
+		var shotSpotter = document.querySelector('#shot-spotter-popup');
+		var drone = document.querySelector('#drone-popup');;
+    el.addEventListener('click', function () {
+          console.log('reopen cards');
+        alpr.setAttribute('visible', true);
+        alprMobile.setAttribute('visible', true);
+        biometric.setAttribute('visible', true);
+        bodyCam.setAttribute('visible', true);
+        camera.setAttribute('visible', true);
+        shotSpotter.setAttribute('visible', true);
+        drone.setAttribute('visible', true);
+    });
+  }
+});
