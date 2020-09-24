34bc1dbb9f04bb8c7dc7d552f002618f0e2ca59e
Artemis Schatzkin <schatzkin@eff.org>
2020-09-24 09:50:59 -0700

    DRYing hint.js

--- js/desktop/hint.js
+++ js/desktop/hint.js
@@ -5,41 +5,23 @@ var alprMobile = document.querySelector('#alpr-mobile-hotspot');
 var biometric = document.querySelector('#biometric-hotspot');
 var bodyCam = document.querySelector('#bodycam-hotspot');
 var camera = document.querySelector('#camera-hotspot');
-var shotSpotter = document.querySelector('#shot-spotter-hotspot');
 var drone = document.querySelector('#drone-hotspot');
+var shotSpotter = document.querySelector('#shot-spotter-hotspot');
 
-function showHotspots() {
+function showHotspots(el) {
   // TODO: DRY this
-  alpr.setAttribute('visible', true);
-  alpr.setAttribute('geometry', { primitive: 'plane', width: 5, height: 5 });
-  alpr.setAttribute('material', { opacity: 1 });
-  alpr.setAttribute('mixin', 'spotted' );
-  alprMobile.setAttribute('visible', true);
-  alprMobile.setAttribute('geometry', { primitive: 'plane', width: 5, height: 5 });
-  alprMobile.setAttribute('material', { opacity: 1 });
-  alprMobile.setAttribute('mixin', 'spotted' );
-  biometric.setAttribute('visible', true);
-  biometric.setAttribute('geometry', { primitive: 'plane', width: 5, height: 5 });
-  biometric.setAttribute('material', { opacity: 1 });
-  biometric.setAttribute('mixin', 'spotted' );
-  bodyCam.setAttribute('visible', true);
-  bodyCam.setAttribute('geometry', { primitive: 'plane', width: 5, height: 5 });
-  bodyCam.setAttribute('material', { opacity: 1 });
-  bodyCam.setAttribute('mixin', 'spotted' );
-  camera.setAttribute('visible', true);
-  camera.setAttribute('geometry', { primitive: 'plane', width: 5, height: 5 });
-  camera.setAttribute('material', { opacity: 1 });
-  camera.setAttribute('mixin', 'spotted' );
-  shotSpotter.setAttribute('visible', true);
-  shotSpotter.setAttribute('geometry', { primitive: 'plane', width: 5, height: 5 });
-  shotSpotter.setAttribute('material', { opacity: 1 });
-  shotSpotter.setAttribute('mixin', 'spotted' );
-  drone.setAttribute('visible', true);
-  drone.setAttribute('geometry', { primitive: 'plane', width: 5, height: 5 });
-  drone.setAttribute('material', { opacity: 1 });
-  drone.setAttribute('mixin', 'spotted' );
+  el.setAttribute('visible', true);
+  el.setAttribute('geometry', { primitive: 'plane', width: 5, height: 5 });
+  el.setAttribute('material', { opacity: 1 });
+  el.setAttribute('mixin', 'spotted' );
 }
 
 hint.addEventListener("click", function(e) {
-  showHotspots();
+  showHotspots(alpr);
+  showHotspots(alprMobile);
+  showHotspots(biometric);
+  showHotspots(bodyCam);
+  showHotspots(camera);
+  showHotspots(drone);
+  showHotspots(shotSpotter);
 });
